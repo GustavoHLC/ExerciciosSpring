1a2074d5550167d6757d32408b728019ad404935
GustavoHLC <gutohenri_car@hotmail.com>
2021-06-30 14:57:40 -0300

    Lista de Exercícios

---
+++ MinhaLojaDeGames/src/main/java/com/minhalojadegames/repository/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package com.minhalojadegames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.minhalojadegames.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	public List<Produto> findAllByTituloContainingIgnoreCase (String titulo);
+}
